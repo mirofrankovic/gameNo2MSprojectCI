1be6119ef966ad3d7bb7e46ea1471dc21ccf88d6
Miro Frankovic <miro.frankovic@gmail.com>
2020-11-30 02:09:24 +0000

    Modify index.html and add application JSON file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,28 @@
 /////////////////////////////////Initialization/////////////////////////////////////////
+$(document).ready(function(){
+    let questionNumber=0;
+    let score=0;
+    let numberOfQuestions;
+
+
+
+    $.getJSON('data/application.json', function(data){
+        console.log(data);
+
+        for(i=0;i<data.questionary.length;i++){
+
+
+        }
+
+
+
+
+    })//JSON
+
+
+
+
+
+});
+
+console.log("Hello World");
