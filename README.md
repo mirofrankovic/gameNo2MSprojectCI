c5d904421a42e0402ac4e72434c577b57b384950
Miro Frankovic <miro.frankovic@gmail.com>
2020-12-04 00:07:33 +0000

    Add structure to Readme file and modify Json functionality.

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # Riddle Me This game
+
+# UX
+
+## Project Goals
+
+## User Stories
+
+## Design
+
+## Wireframes
+
+# Features
+
+## Existing Features
+
+## Features Left to Implement
+
+# Information Architecture
+
+## Database Choice
+
+## Data Modeling
+
+# Technologies Used
+
+## Languages
+
+## Libraries and Frameworks
+
+## Tools
+
+## Databases
+
+# Testing
+
+# Deployment
+
+## Local Deployment
+
+## Heroku Deployment
+
+# Credits
+
+## Code
+
+## Content and Media
+
+## Acknowledgements
+
+# Disclaimer
+
+
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,76 @@
 /////////////////////////////////Initialization/////////////////////////////////////////
-$(document).ready(function(){
-    let questionNumber=0;
-    let score=0;
-    let numberOfQuestions;
+const QUIZ_API = 'application.json';
+const RESULT_API = 'result.json';
 
+        let currentQuestionNo=0;
+        let score=0;    //    user current score
+        let maxScore=0;
+        let answerSubmited=false;
 
-
-    $.getJSON('data/application.json', function(data){
-        console.log(data);
-
-        for(i=0;i<data.questionary.length;i++){
-
-
+ function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");  // assets/data/application.json
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
         }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("assets/data/application.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});       
 
 
 
+function loadQuestion (){
+
+    $(document).ready(function(){
+    $.getJSON('assets/data/application.json', function(data){                
+        // console.log(data);
+
+        for(i=0;i<data.questionary.length;i++){                 
+            var data= questionary[i] 
+            
+            // console.log(questionary.question);
+        }
 
     })//JSON
 
-
-
-
-
 });
 
-console.log("Hello World");
+}
+
+loadQuestion();
+
+// $.getJSON("assets/data/application.json", function(json) {
+//     console.log(json); // this will show the info it in console
+// });
+
+
+   // Create XMLHttpRequest object. ###### This is to road full file on the screen and create a table
+    // var oXHR = new XMLHttpRequest();
+
+    // // Initiate request.
+    // oXHR.onreadystatechange = reportStatus;
+    // oXHR.open("GET", "assets/data/application.json", true);  // get json file.
+    // oXHR.send();
+
+    // function reportStatus() {
+    //     if (oXHR.readyState == 4) {		// Check if request is complete.
+    //         // Write data to a DIV element.
+    //         document.getElementById('question').innerHTML = this.responseText;
+    //     }
+    // }
+
+
+    function scoreGame(){
+
+
+         document.getElementById("after-submit").style.visibility = "visible";
+         document.getElementById("number-total-score").innerHTML = "Your Total Score Is: " + score + "score. ";
+    }
