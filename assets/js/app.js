79925758ea0c4c2436862ffc34fbf931d5b895db
Miro Frankovic <miro.frankovic@gmail.com>
2020-12-05 00:40:08 +0000

    Made parse with the incoming data into JSON with fetch.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,76 +1,84 @@
 /////////////////////////////////Initialization/////////////////////////////////////////
-const QUIZ_API = 'application.json';
-const RESULT_API = 'result.json';
-
-        let currentQuestionNo=0;
-        let score=0;    //    user current score
-        let maxScore=0;
-        let answerSubmited=false;
-
- function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");  // assets/data/application.json
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
-//usage:
-readTextFile("assets/data/application.json", function(text){
-    var data = JSON.parse(text);
-    console.log(data);
-});       
+let gameData=[];        // my global data
+let currentQuestionIndex=0;
+let score=0;              //    user current score
+let maxScore=0;
+let answerSubmited=true;
+let showQuestion=true;
 
 
 
+
+
+$(document).ready(function(){
+/**
+   * Fetches game data from json file and writes response to global variable
+   */
+
+   function getGameData(){
+       fetch("assets/data/gameData.json'")
+       .then(response => {
+           return response.json();            // parse
+       })
+       .then(fetchedData => {
+           gameData = fetchedData;
+       })
+       .catch(error => {
+           alert("Failed to get game data.");
+           console.log(error);
+           });
+
+   }
+
+
+function loadingGame() {
+    $("#main-page").css("background-color", "red"
+    // "url(https://images.app.goo.gl/mRFUenjEXmQ3NWws7)" // json file 
+    );
+    $("#after-submit").hide();
+    $("#game-show").show();
+}  
+
+
+   
 function loadQuestion (){
 
-    $(document).ready(function(){
-    $.getJSON('assets/data/application.json', function(data){                
-        // console.log(data);
-
-        for(i=0;i<data.questionary.length;i++){                 
-            var data= questionary[i] 
+        // for(i=0;i<data.questionary.length;i++){                 
+        //     var data= questionary[i] 
             
-            // console.log(questionary.question);
-        }
-
-    })//JSON
-
-});
+        //     // console.log(questionary.question);
+        // }
 
 }
 
 loadQuestion();
 
-// $.getJSON("assets/data/application.json", function(json) {
-//     console.log(json); // this will show the info it in console
-// });
 
 
-   // Create XMLHttpRequest object. ###### This is to road full file on the screen and create a table
-    // var oXHR = new XMLHttpRequest();
-
-    // // Initiate request.
-    // oXHR.onreadystatechange = reportStatus;
-    // oXHR.open("GET", "assets/data/application.json", true);  // get json file.
-    // oXHR.send();
-
-    // function reportStatus() {
-    //     if (oXHR.readyState == 4) {		// Check if request is complete.
-    //         // Write data to a DIV element.
-    //         document.getElementById('question').innerHTML = this.responseText;
-    //     }
-    // }
 
 
-    function scoreGame(){
+function scoreGame(){
 
 
          document.getElementById("after-submit").style.visibility = "visible";
          document.getElementById("number-total-score").innerHTML = "Your Total Score Is: " + score + "score. ";
     }
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
